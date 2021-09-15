54fe66626a27190322e5c50e67b4fe67fc08b87a
Jan Biniok <jan.biniok@raritan.com>
2021-09-16 01:50:22 +0200

    Add `require-attribute-space-prefix` rule (#6)

---
+++ tests/lib/rules/require-attribute-space-prefix.js
@@ -0,0 +1,51 @@
+var rule = require('..')['require-attribute-space-prefix'];
+var RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run('require-attribute-space-prefix', rule, {
+  valid: [
+    `//  ==UserScript==
+    //  @name         Bottom Padding to Swagger UI
+    //  @namespace    https://github.com/Yash-Singh1/UserScripts
+    //  @version      1.3
+    //  @description  Adds bottom padding to the Swagger UI
+    //  @author       Yash Singh
+    //  @match        https://*/*
+    //  @match        http://*/*
+    //  @icon         https://petstore.swagger.io/favicon-32x32.png
+    //  @grant        none
+    //  @homepage     https://github.com/Yash-Singh1/UserScripts/tree/main/Bottom_Padding_to_Swagger_UI#readme
+    //  @homepageURL  https://github.com/Yash-Singh1/UserScripts/tree/main/Bottom_Padding_to_Swagger_UI#readme
+    //  @supportURL   https://github.com/Yash-Singh1/UserScripts/issues
+    //  @downloadURL  https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/Bottom_Padding_to_Swagger_UI/Bottom_Padding_to_Swagger_UI.user.js
+    //  @updateURL    https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/Bottom_Padding_to_Swagger_UI/Bottom_Padding_to_Swagger_UI.user.js
+    //  ==/UserScript==`,
+  ],
+  invalid: [
+    {
+      code: `// ==UserScript==
+      // @name hello
+      //@description invalid description
+      // ==/UserScript==`,
+      errors: [
+        {
+          messageId: 'attributeNotPrefixedBySpace'
+        }
+      ]
+    },
+    {
+      code: `// ==UserScript==
+      //@name hello
+      //@description invalid description
+      // ==/UserScript==`,
+      errors: [
+        {
+          messageId: 'attributeNotPrefixedBySpace'
+        },
+        {
+          messageId: 'attributeNotPrefixedBySpace'
+        }
+      ]
+    }
+  ]
+});
