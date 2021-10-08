874a02f119e374d58c5b90b89501489bc4ea3d11
Yash-Singh1 <saiansh2525@gmail.com>
2021-10-07 20:40:16 -0700

    Breaking: Add support for localization in require-name
    Change the createValidator API
    Added intellisense to createValidator
    Disable many ESLint Unicorn Rules

--- tests/lib/rules/require-name.js
+++ tests/lib/rules/require-name.js
@@ -35,6 +35,105 @@ ruleTester.run('require-name', rule, {
       // @description This is my description
       // ==/UserScript==`,
       errors: [{ messageId: 'nameAtBeginning' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @name:en This is my name
+      // ==/UserScript==`,
+      output: `// ==UserScript==
+      // @name:en This is my name
+      // @description This is my description
+      // ==/UserScript==`,
+      errors: [{ messageId: 'nameAtBeginning' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @name:en This is my name
+      // @name:es This is my name
+      // ==/UserScript==`,
+      output: `// ==UserScript==
+      // @name:en This is my name
+      // @name:es This is my name
+      // @description This is my description
+      // ==/UserScript==`,
+      errors: [{ messageId: 'nameAtBeginning' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @name:es This is my name
+      // @name This is my name
+      // ==/UserScript==`,
+      output: `// ==UserScript==
+      // @name This is my name
+      // @name:es This is my name
+      // @description This is my description
+      // ==/UserScript==`,
+      errors: [{ messageId: 'nameAtBeginning' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @name:es This is my name
+      // @name:es This is my name
+      // ==/UserScript==`,
+      output: `// ==UserScript==
+      // @name:es This is my name
+      // @name:es This is my name
+      // @description This is my description
+      // ==/UserScript==`,
+      errors: [{ messageId: 'nameAtBeginning' }, { messageId: 'multipleNames' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @name:es This is my name in Spanish
+      // @name This is my name
+      // @version 1.0.0
+      // @name:en This is my name in English
+      // ==/UserScript==`,
+      output: `// ==UserScript==
+      // @name This is my name
+      // @name:es This is my name in Spanish
+      // @name:en This is my name in English
+      // @description This is my description
+      // @version 1.0.0
+      // ==/UserScript==`,
+      errors: [{ messageId: 'nameAtBeginning' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @name:es This is my name in Spanish
+      // @name This is my name
+      // @version 1.0.0
+      // @name:en This is my name in English`,
+      output: `// ==UserScript==
+      // @name This is my name
+      // @name:es This is my name in Spanish
+      // @name:en This is my name in English
+      // @description This is my description
+      // @version 1.0.0`,
+      errors: [{ messageId: 'nameAtBeginning' }]
+    },
+    {
+      code: `// ==UserScript==
+      // @description This is my description
+      // @description This is my description2
+      // @name:es This is my name in Spanish
+      // @name This is my name
+      // @version 1.0.0
+      // @name:en This is my name in English`,
+      output: `// ==UserScript==
+      // @name This is my name
+      // @name:es This is my name in Spanish
+      // @name:en This is my name in English
+      // @description This is my description
+      // @description This is my description2
+      // @version 1.0.0`,
+      errors: [{ messageId: 'nameAtBeginning' }]
     }
   ]
 });
